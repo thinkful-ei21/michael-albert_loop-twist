6d40d9a73af00233291a439047be05f0c91deed5
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-08 09:44:30 -0700

    added max-min.js

---
+++ max-min.js
@@ -0,0 +1,32 @@
+/**
+ * Two functions take a single argument: an array of numbers called numbers. 
+ * max(numbers) should return the largest number in the array, 
+ * while min(numbers) should return the smallest
+ */
+
+ function max(numbers) {
+    let currentNum = numbers[0];
+    let i = 0;
+    while (i < numbers.length) {
+        if (numbers[i] > currentNum) {
+            currentNum = numbers[i];
+         }
+         i++;
+    }
+    return currentNum;
+ }
+
+ function min(numbers) {
+    let currentNum = numbers[0];
+    let i = 0;
+    while (i < numbers.length) {
+        if (numbers[i] < currentNum) {
+            currentNum = numbers[i];
+         }
+         i++;
+    }
+    return currentNum;
+ }
+
+console.log(`Max: ${max([1,5,3,4022,1,19,44,222])}`);
+console.log(`Min: ${min([5,3,4022,1,19,44,222])}`);
