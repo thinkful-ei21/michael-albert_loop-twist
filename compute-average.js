5717ef1da497c72eff5b20483eda1ed6b1b6920e
Albert Sare <thesundaymailman@gmail.com>
2018-05-08 10:02:54 -0700

    added compute-average.js

---
+++ compute-average.js
@@ -0,0 +1,18 @@
+/** Write a function called average that takes an array of numbers as its input and returns the average generated from the array of numbers. */
+
+'use strict';
+
+function average(numbers) {
+    let sum = 0;
+    // use Array.forEach to aggregate a sum
+    numbers.forEach(each => {
+        sum += each;
+    });
+
+    // returning average
+    return sum / numbers.length;
+}
+
+const numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(average(numList1));
